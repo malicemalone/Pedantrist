0f710284beb7719e83d87185d5558996fb0f6707
Alice Malone <m.alice.malone@gmail.com>
2015-12-29 22:09:28 -0800

    adds views, logins, and ability to edit account information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def index
+    @user = User.find(session[:user_id])
+  end
+
   def new
     @user = User.new
   end
@@ -8,9 +12,10 @@ class UsersController < ApplicationController
     @user = User.new(username: params["user"][:username], email: params["user"][:email])
     @user.password=(params["user"][:password_hash])
     if @user.save
-      redirect_to user_path(@user)
+      session[:user_id] = @user.id
+      redirect_to users_path
     else
-      session[:errors] = "Email or usernamme already taken"
+      session[:errors] = "Email or username already taken"
       redirect_to new_user_path
     end
   end
@@ -25,13 +30,32 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(session[:user_id])
-    @user.update_attributes(params)
-    if @user.save
-      redirect_to users_path
+    new_username = params["user"]["username"]
+    new_password = params["user"]["password_hash"]
+    if !!new_username
+      @user.update_attributes(username: new_username)
+      if @user.save
+        session[:succ_username] = "Username updated!"
+      else
+        session[:errors] = "Too short or already in use"
+      end
+        redirect_to edit_user_path(@user)
     end
+    if !!new_password
+      @user.update_attributes(password: new_password)
+      if @user.save
+        @user.password=(new_password)
+        session[:succ_pass] = "Password updated!"
+      end
+        redirect_to edit_user_path(@user)
+    end
+
   end
 
   def destroy
+    p "GOT HERE"
+    session.delete(:user_id)
+    redirect_to welcome_index_path
   end
 
 end
